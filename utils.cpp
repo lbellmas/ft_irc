3ddc6ea06897881c38a57d20ba1d8d7057358469
Sami Himi Ben Alilou <shimi-be@cbr15s4.42barcelona.com>
2026-08-29 18:15:24 +0200

    Fix Join, only need to do MODE and TOPIC

--- utils.cpp
+++ utils.cpp
@@ -31,13 +31,12 @@ void cmdMsg(IRCmd command, Client *c, Server *s){
     if (command.params[0][0] == '#'){
         Channel *chan;
         std::string name = command.params[0];
-        if((chan = s->searchChannel(command.params[0].erase(0,1))) != NULL){
-            if (chan->hasClient(c->getNick())){
-                std::string message = c->getPrefix().append("PRIVMSG " + name+ " :").append(command.params[1]).append("\r\n");
-                std::cout << "Ms " << message << std::endl;
+        if((chan = s->searchChannel(command.params[0])) != NULL){
+            if (chan->hasClient(c->getNick())){ 
+                std::string message = c->getPrefix().append(" " + command.cmd + " " + command.params[0] + " :" + command.params[1]).append("\r\n");
                 std::vector<std::string> clients = chan->getClients();
                 for(size_t i = 0; i < clients.size(); i++){
-                    //if (clients[i] != c->getNick())
+                    if (clients[i] != c->getNick())
                         s->sendMessage(message, s->searchClient(clients[i])->getFd());
                 }
             } else {
@@ -50,9 +49,7 @@ void cmdMsg(IRCmd command, Client *c, Server *s){
         Client *reciever;
         std::cout << "Message " << command.params[1] << std::endl;
         if ((reciever = s->searchClient(command.params[0])) != NULL){
-            std::cout << "Reciever name :" << reciever->getNick() << std::endl;
             std::string message = c->getPrefix().append("PRIVMSG " + reciever->getNick() + " :").append(command.params[1]).append("\r\n");
-            std::cout << "mssg" << message << std::endl;
             s->sendMessage(message, reciever->getFd());
         } else {
             s->sendMessage("Error sending message! Nick not in use", reciever->getFd());
@@ -61,19 +58,22 @@ void cmdMsg(IRCmd command, Client *c, Server *s){
 }
 void cmdJoin(IRCmd command, Client *c, Server *s){ 
     Channel *chan;
-    if((chan = s->searchChannel(command.params[0].erase(0,1))) != NULL){
-        std::cout << "JOINED channel : " << chan->getName() << std::endl;
-        std::cout << chan ->getNumUsers() << "    " << chan->getUserLimit() << "\n";
+    if((chan = s->searchChannel(command.params[0])) != NULL){
         if (chan->getNumUsers() + 1 <= chan->getUserLimit() || chan->getUserLimit() == 0){
             if (chan->isInviteOnly()){
                 if (chan->isInvited(c->getNick())){
                     chan->addClient(c->getNick());
+                    std::string message = c->getPrefix() + command.cmd + " " + command.params[0].append("\r\n");
+                    s->sendMessage(message, c->getFd());
                 } else {
                     s->sendMessage("ERROR, TRYING TO JOIN INVITE ONLY CHANNEL", c->getFd());
                 }
                 } else {
-                    if (!chan->hasClient(c->getNick()))
+                    if (!chan->hasClient(c->getNick())){
                         chan->addClient(c->getNick());
+                        std::string message = c->getPrefix() + command.cmd + " " + command.params[0].append("\r\n");
+                        s->sendMessage(message, c->getFd());
+                    }
                     else {
                         s->sendMessage("ERROR, USER ALREADY IN CHANNEL", c->getFd());
                     }
@@ -84,17 +84,18 @@ void cmdJoin(IRCmd command, Client *c, Server *s){
     } else {
         std::cout << "Created Channel!" << std::endl;
         Channel Ch(command.params[0]);
-        std::cout << Ch.getName() << std::endl;
         Ch.addClient(c->getNick());
         Ch.addOperator(c->getNick());
         s->addChannel(Ch);
+        std::string message = c->getPrefix() + command.cmd + " " + command.params[0].append("\r\n");;
+        s->sendMessage(message, c->getFd());
     }
 }
 
 void cmdMode(IRCmd command, Client *c, Server *s){
     if (command.params[0][0] == '#'){
         Channel *ch;
-        if ((ch = s->searchChannel(command.params[0].erase(0,1))) != NULL){
+        if ((ch = s->searchChannel(command.params[0])) != NULL){
             if (ch->isOperator(c->getNick())){
                 ch->changeModes(command, c, s);
             } else if (ch->hasClient(c->getNick())){
